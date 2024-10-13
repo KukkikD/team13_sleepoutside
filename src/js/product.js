885354e56e9b6b7c592bbcd8d0d66b9dc23d5aa0
KukkikD <adeezon9@gmail.com>
2024-10-13 02:01:52 -0700

    update

--- src/js/product.js
+++ src/js/product.js
@@ -2,12 +2,10 @@ import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-// Use getParam to Retrieve the Product ID
+loadHeaderFooter();
+
+const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-// Created instance of the ProductData class
-const dataSource = new ProductData("tents");
-
-// Created instance of the ProductDetails class
 const product = new ProductDetails(productId, dataSource);
 product.init();
